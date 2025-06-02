91116992c13dd0fc862694d6f7493e09ed8dee61
ishaanm09 <your.github.email@example.com>
2025-06-02 17:58:32 -0400

    Add environment variable configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    PYTHON_API_URL: process.env.PYTHON_API_URL,
+  },
 }
 
 module.exports = nextConfig 
